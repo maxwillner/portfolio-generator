6062abe438e856e34a8e0f237d9ded5baefcca78
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-02 16:43:41 -0400

    added validation check for answers

--- app.js
+++ app.js
@@ -15,12 +15,27 @@ const promptUser = () => {
         {
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is your name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "github",
-            message: "Enter your GitHub Username"
+            message: "Enter your GitHub Username (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub Username!")
+                }
+            }
         },
         {
             type: "input",
@@ -52,7 +67,15 @@ Add New Project
         {
             type: "input",
             name: "description",
-            message: "Provide a description of the project (Required)"
+            message: "Provide a description of the project (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of your project!");
+                    return false;
+                }
+            }
         },
         {
             type: "checkbox",
@@ -63,7 +86,15 @@ Add New Project
         {
             type: "input",
             name: "link",
-            message: "Enter the GitHub link to your project. (Required)"
+            message: "Enter the GitHub link to your project. (Required)",
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the GitHub link to your project!");
+                    return false;
+                }
+            }
         },
         {
             type: "confirm",
